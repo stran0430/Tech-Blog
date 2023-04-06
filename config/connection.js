dc6e439d89a5be9ea1c9476a132c49c5028d2bec
Steve Tran <Stran0430@gmail.com>
2023-04-06 01:10:21 -0400

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URI);
+const sequelize = new Sequelize(process.env.MYSQL_URI){
+    dialect: mysql,
+
+};
 
 module.exports = sequelize;
