cad32bc1e59cbc69ed20b82375ecff29224f01ab
Steve Tran <Stran0430@gmail.com>
2023-04-05 22:23:28 -0400

    revert config back

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-let sequelize;
-
-if (process.env.MYSQL_URI) {
-  sequelize = new Sequelize(process.env.MYSQL_URI);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+const sequelize = new Sequelize(process.env.MYSQL_URI);
 
 module.exports = sequelize;
