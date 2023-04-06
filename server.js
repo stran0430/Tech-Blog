31fb58f46313fd5b3b51d8f61eeba5bd5cfb48d1
Steve Tran <Stran0430@gmail.com>
2023-04-05 20:29:04 -0400

    added session secret password

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "Super secret secret",
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
