cd506ace8343715b2c227c40839d0d44cb982700
Steve Tran <Stran0430@gmail.com>
2023-04-05 22:21:32 -0400

    change config because heroku was not working

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,21 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URI);
+let sequelize;
+
+if (process.env.MYSQL_URI) {
+  sequelize = new Sequelize(process.env.MYSQL_URI);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
