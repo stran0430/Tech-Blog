1956de83b53ea30e2f990cbc4ad6ef0af509a3b3
Steve Tran <Stran0430@gmail.com>
2023-04-06 14:49:20 -0400

    revert accidental changes

--- models/index.js
+++ models/index.js
@@ -1,43 +1,22 @@
-'use strict';
+const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./Comment");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
-module.exports = db;
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Blog, Comment };
